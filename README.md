cc8a24c4e63949639e8d694a4cbaf4e373545fa1
Victoria <v.latynina46@gmail.com>
2025-06-06 17:54:17 +0200

    Add openai version

--- README.md
+++ README.md
@@ -1,11 +1,21 @@
 <img src="docs/images/logo.png" alt="Healthcare AI Agent logo" width="160"> 
 
-# Healthcare Agent 
+# WellBe+ Assistant 
 
+WellBe+ AI Agent is a multi-context assistant built with the agents SDK framework from OpenAI. It orchestrates three secure MCP servers: a public drug-knowledge FDA service, a WHOOP biometric feed, and a MySQL clinical data store. Every user question is answered with up-to-date medication data. For example, the agent can answer to more generic questions such as “What are the adverse effects of Prozac?” or more specific ones including complex queries where both FDA and Whoop data can be combined: "I started to take Prozac 3 months ago, do you see any trends in my sleep quality and what are its most frequent side-effects?" The same architecture scales to workout recovery tips, drug interaction cross-checks, or longitudinal trend insights.
 
+## MCP Servers in use
 
+[Whoop](https://smithery.ai/server/@ctvidic/whoop-mcp-server)
 
+[Healthcare MCP with PubMed, FDA and other APIs](https://smithery.ai/server/@Cicatriiz/healthcare-mcp-public)
+
+[MySQL + custom drug interaction](https://smithery.ai/server/mysql-mcp-server)
 
 ## How to start the application
 
-`brew services start mysql`
+If you plan to use our custom database:
+
+- **Install Docker**: Follow the official guide → [Docker Engine installation](https://docs.docker.com/engine/install/) 
+- **Install Docker Compose**: Follow the plugin guide → [Docker Compose installation](https://docs.docker.com/compose/install/) ￼
+- Run `docker-compose up` in the project directory (make sure data/ folder is not empty)
--- app.py
+++ app.py
@@ -1,41 +1,79 @@
-import asyncio
+import gradio as gr
+from gradio import ChatMessage
+import time
 
-from agents import Agent, Runner, set_tracing_disabled
-from agents.extensions.models.litellm_model import LitellmModel
-from agents.mcp.server import MCPServerStdio, MCPServerStreamableHttp
-
-from config import PROMPT_TEMPLATE, MCP_CONFIGS, MODEL_API_KEY
-
-
-set_tracing_disabled(disabled=True)
-
-
-async def main(model: str = "mistral/mistral-large-latest"):
-    healthcare_server = MCPServerStreamableHttp(
-        params=MCP_CONFIGS["healthcare-mcp-public"], name="Healthcare MCP Server"
+def generate_response(history):
+    history.append(
+        ChatMessage(
+            role="user", content="What is the weather in San Francisco right now?"
+        )
     )
-    mysql_server = MCPServerStdio(
-        params=MCP_CONFIGS["mysql"],
-            name="MySQL MCP Server"
+    yield history
+    time.sleep(0.25)
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="In order to find the current weather in San Francisco, I will need to use my weather tool.",
         )
-    whoop_server = MCPServerStdio(params=MCP_CONFIGS["whoop"], name="Whoop MCP Server")
+    )
+    yield history
+    time.sleep(0.25)
 
-    async with healthcare_server as hserver, mysql_server as mysql, whoop_server as whoop:
-        # Create the agent with the connected servers
-        agent = Agent(
-            name="Assistant",
-            instructions=PROMPT_TEMPLATE,
-            model=LitellmModel(model=model, api_key=MODEL_API_KEY),
-            mcp_servers=[
-                hserver,
-                whoop,
-                mysql
-            ],
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="API Error when connecting to weather service.",
+            metadata={"title": "💥 Error using tool 'Weather'"},
         )
-        # Run the agent with a sample query
-        result = await Runner.run(agent, "what are adverse effects of prozac?")
-        print(result.final_output)
+    )
+    yield history
+    time.sleep(0.25)
 
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="I will try again",
+        )
+    )
+    yield history
+    time.sleep(0.25)
+
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="Weather 72 degrees Fahrenheit with 20% chance of rain.",
+            metadata={"title": "🛠️ Used tool 'Weather'"},
+        )
+    )
+    yield history
+    time.sleep(0.25)
+
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="Now that the API succeeded I can complete my task.",
+        )
+    )
+    yield history
+    time.sleep(0.25)
+
+    history.append(
+        ChatMessage(
+            role="assistant",
+            content="It's a sunny day in San Francisco with a current temperature of 72 degrees Fahrenheit and a 20% chance of rain. Enjoy the weather!",
+        )
+    )
+    yield history
+
+def like(evt: gr.LikeData):
+    print("User liked the response")
+    print(evt.index, evt.liked, evt.value)
+
+with gr.Blocks() as demo:
+    chatbot = gr.Chatbot(type="messages", height=500, show_copy_button=True)
+    button = gr.Button("Get San Francisco Weather")
+    button.click(generate_response, chatbot, chatbot)
+    chatbot.like(like)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    demo.launch()
